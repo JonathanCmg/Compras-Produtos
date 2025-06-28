26f94f9f689d7cc1c018d890c42e42b40ee61047
Aluno <Aluno@10.60.2.229>
2025-06-27 21:02:14 -0300

    Metodo salvar,editar e excluir de produtos

---
+++ src/main/java/br/edu/compras02/model/Produto.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.compras02.model;
+
+public class Produto {
+    private String nome;
+    private int quantidade;
+    private double preco;
+    private int codigo;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, int quantidade, double preco, int codigo) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "nome=" + nome + ", quantidade=" + quantidade + ", preco=" + preco + ", codigo=" + codigo + '}';
+    }
+    
+    
+    
+    
+}
+
